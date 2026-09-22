bbf8826fd808b890873f1ba47a145c0a632ccd1d
Alanis Lemaitre <alanislemaitre@gmail.com>
2026-09-21 20:48:17 -0600

    Agrega serializers y views

--- clinica/urls.py
+++ clinica/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #
+    path('', views.api_activa),
+
+    path('api/propietarios/', views.api_propietarios),
+    path('api/mascotas/', views.api_mascotas),
+    path('api/mascotas/<int:pk>/', views.detalle_mascota),
+    path('api/consultas/', views.api_consultas),
 ]
--- clinica/views.py
+++ clinica/views.py
@@ -1,4 +1,95 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Propietario, Mascota, ConsultaVeterinaria
+from .serializers import (
+    PropietarioSerializer,
+    MascotaSerializer,
+    ConsultaVeterinariaSerializer,
+)
 
+
+# Propietarios
+# ------------------------------------------------------------------
+@api_view(['GET', 'POST'])
+def api_propietarios(request):
+    if request.method == 'GET':
+        propietarios = Propietario.objects.all().order_by('id')
+        serializer = PropietarioSerializer(propietarios, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = PropietarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Mascotas
+# ------------------------------------------------------------------
+@api_view(['GET', 'POST'])
+def api_mascotas(request):
+    if request.method == 'GET':
+        mascotas = Mascota.objects.all().order_by('id')
+        serializer = MascotaSerializer(mascotas, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MascotaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def detalle_mascota(request, pk):
+    try:
+        mascota = Mascota.objects.get(pk=pk)
+    except Mascota.DoesNotExist:
+        return Response(
+            {'error': 'Mascota no encontrada'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if request.method == 'GET':
+        serializer = MascotaSerializer(mascota)
+        return Response(serializer.data)
+
+    if request.method in ['PUT', 'PATCH']:
+        serializer = MascotaSerializer(
+            mascota,
+            data=request.data,
+            partial=(request.method == 'PATCH')
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        mascota.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Consultas veterinarias
+# ------------------------------------------------------------------
+@api_view(['GET', 'POST'])
+def api_consultas(request):
+    if request.method == 'GET':
+        consultas = ConsultaVeterinaria.objects.all().order_by('id')
+        serializer = ConsultaVeterinariaSerializer(consultas, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = ConsultaVeterinariaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def api_activa(request):
+    return HttpResponse('API de Gestión Veterinaria activa')
